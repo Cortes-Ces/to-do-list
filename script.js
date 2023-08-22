49909d1fbf2dc2e18463404ee65c5bf06ff29d25
Cortes-Ces <Cortesces90@gmail.com>
2023-08-22 15:25:30 -0400

    Added function to add and remove items on todo list

---
+++ script.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const todosUL = document.getElementById('todos')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addTodo()
+})
+
+function addTodo(todo) {
+    let todoText = input.value
+
+    if(todo) {
+        todoText = todo.text
+    }
+
+    if(todoText) {
+        const todoEl = document.createElement('li')
+        if (todo && todo.completed) {
+            todoEl.classList.add('completed')
+        }
+
+        todoEl.innerText = todoText
+
+        // When list is clicked, it marks as completed
+        todoEl.addEventListener('click', () => todoEl.classList.toggle('completed'))
+
+        // When right clicked, removes list
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault()
+            
+            todoEl.remove()
+        })
+
+        todosUL.appendChild(todoEl)
+
+        input.value = ''
+    }
+    
+}
